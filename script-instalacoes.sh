fd24eefafa37c5471976bbae827992ae83a29b36
Felipe Amaral <amaral.felipe@live.com>
2020-03-06 09:56:55 -0300

    Adding script-instalacoes.sh file

---
+++ script-instalacoes.sh
@@ -0,0 +1,33 @@
+echo "Atualizando repositórios.."
+if ! apt-get update -y
+then
+    echo "Erro na atualização. Verifique seu arquivo /etc/apt/sources.list"
+    exit 1
+fi
+echo "Atualização feita com sucesso"
+
+echo "Atualizando pacotes já instalados..."
+if ! apt-get dist-upgrade -y
+then
+    echo "Não foi possível atualizar pacotes."
+    exit 1
+fi
+echo "Atualização de pacotes feita com sucesso"
+
+echo "Removendo versões antigas já instaladas..."
+if [ ! -z $(which -- docker) ]; then
+    sudo apt-get remove docker docker-engine docker.io
+fi 
+
+echo "Baixando e instalando os recursos..."
+sudo apt-get install -y git npm python python-pip golang docker.io 
+sudo npm install -g npm n
+sudo -H python -m pip install --upgrade pip
+sudo systemctl start docker
+sudo systemctl enable docker
+printf "\n[http]
+        sslVerify = false
+        \n[user]
+        name = Felipe Amaral
+        email=amaral.felipe@live.com" | tee -a $HOME/.gitconfig && \
+printf "\nexport GOPATH=\$(go env GOPATH)" | tee -a $HOME/.zshrc $HOME/.bashrc
