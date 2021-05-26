14ed24e298b3cc696ba97ef3d8ffe7cb93d50edd
Felipe Amaral <amaral.felipe@live.com>
2021-05-26 08:16:17 -0300

    Finish script base for ubuntu

--- ubuntu-code-anywhere/script.sh
+++ ubuntu-code-anywhere/script.sh
@@ -3,10 +3,10 @@ apt-get install wget -y && \
 apt-get install snapd -y && \
 apt-get install git-all -y && \
 apt-get install terminator -y && \
-update-alternatives --config x-terminal-emulator
+update-alternatives --config x-terminal-emulator && \
 apt-get install zsh -y && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
-apt-get install ranger caca-utils highlight atool w3m poppler-utils mediainfo && \
+apt-get install ranger caca-utils highlight atool w3m poppler-utils mediainfo -y && \
 apt-get updade -y && \
 apt install apt-transport-https ca-certificates curl software-properties-common -y && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
@@ -18,10 +18,9 @@ apt install ./keybase_amd64.deb -y && \
 apt install apt-transport-https curl -y && \
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && \
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list && \
-apt update && \
+apt get-update && \
 apt install brave-browser -y && \
-snap install brave -y && \
-apt update -y && \
+apt get-update -y && \
 apt-get install software-properties-common && \
 apt-add-repository ppa:remmina-ppa-team/remmina-next && apt update && apt install remmina remmina-plugin-rdp remmina-plugin-secret && \
 wget https://download.anydesk.com/linux/anydesk_6.1.0-1_amd64.deb -O anydesk.deb && \
